34cc4a51e675cf5676eec7d7fd2096f5e423f5ac
paul-laird <35932444+paul-laird@users.noreply.github.com>
2020-03-31 19:35:00 +0100

    Update portfolio.py
    Added connection to API and ValueError checks

--- portfolio.py
+++ portfolio.py
@@ -1,13 +1,19 @@
-prices={'MSFT':160,'AMZN':200}
+def getPrice(sym):
+    results=requests.get('https://query1.finance.yahoo.com/v8/finance/chart/%s'%sym)
+    return json.loads(results.content)['chart']['result'][0]['meta']['regularMarketPrice']
 
 class Portfolio:
     def __init__(self):
         self.__balance={}
         
     def invest(self,sym,qty):
+        if qty<0:
+            raise ValueError()
         self.__balance[sym]=self.getBalance(sym)+qty
             
     def divest(self,sym,qty):
+        if qty<0 or qty>self.__balance[sym]:
+            raise ValueError()
         self.__balance[sym]=self.getBalance(sym)-qty
             
     def getBalance(self,sym):
@@ -18,5 +24,5 @@ class Portfolio:
     def value(self):
         total=0
         for k,v in self.__balance.items():
-            total+=v*prices[k]
+            total+=v*getPrice(k)
         return total
